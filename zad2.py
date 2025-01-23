027491f0ee9f86b73e567f13e8a4f2522ce1c4ce
peterlesidrenski <148121354+peterlesidrenski@users.noreply.github.com>
2025-01-23 21:31:28 +0200

    Add files via upload

--- zad2.py
+++
@@ -1,7 +0,0 @@
-numbers = tuple(map(int, input().split()))
-max_number = max(numbers)
-min_number = min(numbers)
-difference = max_number - min_number
-print(f"max {max_number}")
-print(f"min {min_number}")
-print(f"difference {difference}")
